94d554eb14bf0b9cf0804f639e2bbac5ae2f493f
黄玮鹏 <chinawphuang@163.com>
2018-05-28 00:41:06 +0800

    Update

--- websrc/app/scripts/component/batch/batchController.js
+++ websrc/app/scripts/component/batch/batchController.js
@@ -39,6 +39,21 @@ angular.module('npmtApp').controller('batchController',['$http', '$rootScope', '
         });
     }
 
+    $scope.openBatchInfoModal = function(batch) {
+        $scope.updatedProductBatch = {
+            "amount": batch.amount,
+            "averageAmount": batch.averageAmount,
+            "description": batch.description,
+            "name": batch.name,
+            "wechatUserTitle":batch.wechatUserTitle,
+            "wechatUserText":batch.wechatUserText,
+            "wechatShareTitle":batch.wechatShareTitle,
+            "wechatShareLink":batch.wechatShareLink,
+            "wechatShareImgUrl":batch.wechatShareImgUrl
+        };
+        $('#'+batch.id+'_batch_info').modal('show');
+    }
+
     $scope.openDetailModal = function(productDetail){
         $('#'+productDetail[0].productId+'_product_detail').modal('show')
     }
@@ -68,9 +83,14 @@ angular.module('npmtApp').controller('batchController',['$http', '$rootScope', '
         "forceCityCheck": false,
         "randomRedpacket":false,
         "randomMinAmount": 1,
-        "randomMaxAmount": 1000
+        "randomMaxAmount": 1000,
+        "wechatUserTitle":"",
+        "wechatUserText":"",
+        "wechatShareTitle":"",
+        "wechatShareLink":"",
+        "wechatShareImgUrl":""
     };
-    $scope.addProductBatch = function(tempProductBatch){
+    $scope.addProductBatch = function(tempProductBatch) {
 
         tempProductBatch = {
           "allowSellCities": $scope.getSelectedCityList($scope.outputBrowsers),
@@ -81,7 +101,12 @@ angular.module('npmtApp').controller('batchController',['$http', '$rootScope', '
           "forceCityCheck": $scope.addedProductBatch.forceCityCheck,
           "randomRedpacket": $scope.addedProductBatch.randomRedpacket,
           "randomMinAmount": $scope.addedProductBatch.randomMinAmount,
-          "randomMaxAmount": $scope.addedProductBatch.randomMaxAmount
+          "randomMaxAmount": $scope.addedProductBatch.randomMaxAmount,
+          "wechatUserTitle": $scope.addedProductBatch.wechatUserTitle,
+          "wechatUserText": $scope.addedProductBatch.wechatUserText,
+          "wechatShareTitle": $scope.addedProductBatch.wechatShareTitle,
+          "wechatShareLink": $scope.addedProductBatch.wechatShareLink,
+          "wechatShareImgUrl": $scope.addedProductBatch.wechatShareImgUrl,
         };
 
         batchService.addProductBatchServ(tempProductBatch).then(function(response){
@@ -95,7 +120,61 @@ angular.module('npmtApp').controller('batchController',['$http', '$rootScope', '
               "forceCityCheck": false,
               "randomRedpacket":false,
               "randomMinAmount": 1,
-              "randomMaxAmount": 1000
+              "randomMaxAmount": 1000,
+              "wechatUserTitle":"",
+              "wechatUserText":"",
+              "wechatShareTitle":"",
+              "wechatShareLink":"",
+              "wechatShareImgUrl":""
+            };
+            $('#addProductBatch').modal('hide');
+            $timeout(function () {
+              $state.reload();
+            },800);
+          }
+        }, function (error) {
+          console.log(error);
+        });
+
+    }
+
+
+    $scope.updateProductBatch = function(tempProductBatch) {
+
+        tempProductBatch = {
+          "allowSellCities": $scope.getSelectedCityList($scope.outputBrowsers),
+          "amount": $scope.addedProductBatch.amount,
+          "averageAmount": $scope.addedProductBatch.averageAmount,
+          "description": $scope.addedProductBatch.description || "",
+          "name": $scope.addedProductBatch.name,
+          "forceCityCheck": $scope.addedProductBatch.forceCityCheck,
+          "randomRedpacket": $scope.addedProductBatch.randomRedpacket,
+          "randomMinAmount": $scope.addedProductBatch.randomMinAmount,
+          "randomMaxAmount": $scope.addedProductBatch.randomMaxAmount,
+          "wechatUserTitle": $scope.addedProductBatch.wechatUserTitle,
+          "wechatUserText": $scope.addedProductBatch.wechatUserText,
+          "wechatShareTitle": $scope.addedProductBatch.wechatShareTitle,
+          "wechatShareLink": $scope.addedProductBatch.wechatShareLink,
+          "wechatShareImgUrl": $scope.addedProductBatch.wechatShareImgUrl,
+        };
+
+        batchService.addProductBatchServ(tempProductBatch).then(function(response){
+          if (response.status = 200 ) {
+            $scope.addedProductBatch = {
+              "allowSellCities": [],
+              "amount": null,
+              "averageAmount": null,
+              "description": "",
+              "name": "",
+              "forceCityCheck": false,
+              "randomRedpacket":false,
+              "randomMinAmount": 1,
+              "randomMaxAmount": 1000,
+              "wechatUserTitle":"",
+              "wechatUserText":"",
+              "wechatShareTitle":"",
+              "wechatShareLink":"",
+              "wechatShareImgUrl":""
             };
             $('#addProductBatch').modal('hide');
             $timeout(function () {
@@ -118,7 +197,12 @@ angular.module('npmtApp').controller('batchController',['$http', '$rootScope', '
           "forceCityCheck": false,
           "randomRedpacket":false,
           "randomMinAmount": 1,
-          "randomMaxAmount": 1000
+          "randomMaxAmount": 1000,
+          "wechatUserTitle":"",
+          "wechatUserText":"",
+          "wechatShareTitle":"",
+          "wechatShareLink":"",
+          "wechatShareImgUrl":""
         };
         $('#addProductBatch').modal('hide');
     }
